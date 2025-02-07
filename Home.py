28215a7aaf0dbe3fdffb2181386ea2a4eb362760
Harleen Chadha <harleen.chadha@cginfinity.com>
2025-02-07 11:17:42 +0530

    changes

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from streamlit_extras.app_logo import add_logo
+
+add_logo("logo.png")
+
+st.header("Welcome to InSights")
+st.text("Welcome to InSights, the pinnacle of global decision-making data by CG Infinity. Our cutting-edge data technology solutions compress data discovery to insights, saving time, effort, and budget. Empower individuals and enterprises to explore, visualize, model, and present data for informed decisions and better business outcomes. Revolutionize your world with InSights!")
+st.text("On this website, you can find the source links from which our datasets have been curated.")
+st.text("For each dataset, we provide direct links to the original sources. You can explore the data in detail, verify its authenticity, and understand the context.")
--- app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from streamlit_extras.app_logo import add_logo
-
-add_logo("logo.png")
-
